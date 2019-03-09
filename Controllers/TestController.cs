d8e01beceb32b83fef8b6e696e96d6b74a69bd66
Aleksandar Hristov <ahristov@tu-sofia.bg>
2019-03-09 14:56:01 +0200

    Add files via upload

---
+++ Controllers/TestController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PI_Upr1.Controllers
+{
+    public class Customer
+    {
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public override string ToString()
+        {
+            return this.CustomerName + "|" + this.Address;
+        }
+    }
+
+    public class TestController : Controller
+    {
+        // GET: Test
+        public string GetString()
+        {
+            return "Hello World is old now. It&rsquo;s time for wassup bro ;)";
+        }
+        public Customer GetCustomer()
+        {
+            Customer c = new Customer();
+            c.CustomerName = "Customer 1";
+            c.Address = "Address1";
+            return c;
+        }
+
+        [NonAction]
+        public string SimpleMethod()
+        {
+            return "Hi, I am not action method";
+        }
+
+        public ActionResult GetView()
+        {
+            if (true)
+            {
+                return View("MyView");
+            }
+            else
+            {
+                return Content("Hi Welcome");
+            }
+
+        }
+
+
+    }
+}
